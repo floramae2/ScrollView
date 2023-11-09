37e25453c304797307d4332fb30aa0324e25809b
Flora Mae Requillo <143053138+floramae2@users.noreply.github.com>
2023-11-10 01:34:50 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-web';
+import information from './src/information';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt1}>LIST OF STUDENTS</Text>
+      {information.map((info)=>{
+        return(
+          <View>
+            <ScrollView>
+              <Text style={styles.txt1}>{info.name}</Text>
+              <Text style={styles.txt2}>{info.place}</Text>
+
+            </ScrollView>
+
+          </View>
+        )
+      })}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +28,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: 20,
+    marginTop: 100,
+    marginLeft: 30
   },
+  txt1: {
+    fontWeight: 'bold',
+    fontSize: 18,
+
+  },
+  txt2: {
+
+  }
 });
---
+++ src/information.js
@@ -0,0 +1,67 @@
+import react from "react";
+
+export const information = [
+    {
+        id: 1,
+        name: "BATULANON, RIZA",
+        place: "Tagustusan, Balilihan"
+    },
+    {
+        id: 2,
+        name: "BETASA, MIARAFE",
+        place: "Mahayag, Catigbian"
+    },
+    {
+        id: 3,
+        name: "CUMAHIG, JENMARIE",
+        place: "Libertad, Tubigon"
+    },
+    {
+        id: 4,
+        name: "GARCIA, ROGEL",
+        place: "Boyog Norte, Balilihan"
+    },
+    {
+        id: 5,
+        name: "JAUM, MARIE",
+        place: "Poblacion, Catigbian"
+    },
+    {
+        id: 6,
+        name: "LANSANG, PEARL",
+        place: "San Roque, Balilihan"
+    },
+    {
+        id: 7,
+        name: "LIPARTO, MONALIZA",
+        place: "Boyog Proper, Balilihan"
+    },
+    {
+        id: 8,
+        name: "LUMANTAS, DESERRIE",
+        place: "Hagbuaya, Catigbian"
+    },
+    {
+        id: 9,
+        name: "QUISQUIRIN, JHANNEL",
+        place: "Del Rosario, Balilihan"
+    },
+    {
+        id: 10,
+        name: "REQUILLO, FLORA",
+        place: "Boyog Norte, Balilihan"
+    },
+    {
+        id: 11,
+        name: "TAMAYO, LORAINE",
+        place: "Cambuac Sur, Sikatuna"
+    },
+    {
+        id: 12,
+        name: "VILLOREJO, JAQUELINE",
+        place: "Australia, Maribojoc"
+    },
+    
+]
+
+export default information;
